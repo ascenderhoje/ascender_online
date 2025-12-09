ab62413543b06ba4cc7ebc550e4b388897223ed2
ascenderhoje <ascenderh@gmail.com>
2025-12-09 17:54:39 -0300

    Updated AdminAvaliacaoViewPage.tsx

--- src/pages/AdminAvaliacaoViewPage.tsx
+++ src/pages/AdminAvaliacaoViewPage.tsx
@@ -382,7 +382,7 @@ export function AdminAvaliacaoViewPage() {
   }
 
   return (
-    <>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <div className="space-y-6">
         <div className="flex items-center justify-between">
           <div>
@@ -600,6 +600,6 @@ export function AdminAvaliacaoViewPage() {
         </div>
       )}
       </div>
-    </>
+    </div>
   );
 }
